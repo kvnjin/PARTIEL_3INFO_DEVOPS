533558e3baf3686ec3a231825f11ee424b583904
Kévin Jin <kevin1501@hotmail.fr>
2025-02-26 11:47:52 +0100

    fix: changing app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 def add(a, b):
-    """Ajoute deux nombres."""
     return a + b
 
 
 def multiply(x, y):
-    """Multiplie deux nombres."""
     return x * y
 
 
 def divide(x, y):
-    """Divise deux nombres si le diviseur n'est pas zéro."""
-    beta = 0
     if y != 0:
         return x / y
     return None
 
+
 def greet(name):
-    """Retourne un message de salutation."""
     if name == "":
         return "Hello, World!"
-    else:
-        return "Hello, " + name
+    return "Hello, " + name
