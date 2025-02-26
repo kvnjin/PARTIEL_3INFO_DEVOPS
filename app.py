5756c253a83f2bea02014e77025f3eea1148f5a9
Kévin Jin <kevin1501@hotmail.fr>
2025-02-26 12:03:56 +0100

    retest

--- app.py
+++ app.py
@@ -2,20 +2,17 @@ def add(a, b):
     """Ajoute deux nombres."""
     return a + b
 
+def multiply( x,y ):
+    return x*y
 
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
+def divide(x , y ):
+    beta = 0
     if y != 0:
-        return x / y
-
+        return x/y
 
 def greet(name):
-    # GREET FUNCTION
-    if name == "":
+    # GREET FONCTION
+    if name =="":
         return "Hello, World!"
     else:
-        return "Hello, " + name
-
+       return "Hello,"+name
